07658b86bc3fd1ef6cd2228c773d071f6f8483c2
Yan Almeida <56321281+keys8410@users.noreply.github.com>
2019-12-11 20:53:30 -0300

    version 0.2

--- velha.c
+++ velha.c
@@ -3,13 +3,15 @@
 #include <ctype.h>
 #include <locale.h>
 #include <conio.h>
+#include <time.h>
 #include <windows.h>
 
 #define TAM 7 // dimensionamento
+#define tam 18 // dimensionamento
 
 // Prot. Function
 void showMenu(int n, int x); // menu inicial
-void composition(); // composição final
+void composition(); // composição final >> solo/dual
 void tab(int n); // bordas horizontais >> 1
 void tab2(int n); // bordas horizontais >> 2
 int showTab(int n, int x, int y, char *z); // tabuleiro jogável
@@ -17,7 +19,7 @@ int showTabM(int n, int x, int y, char *z); // tabuleiro visual >> como jogar
 void help(); // como jogar
 
 /* 
-	09 de dezembro de 2019	
+	11 de dezembro de 2019	
     Departamento de T.I., UniProjeção (Taguatinga, Campus I)
     
     Sistemas de Informação (2º semestre)
@@ -33,11 +35,12 @@ void help(); // como jogar
 
 // global vars.
 int i, j, menu;
-char posS[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char posS[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}, res; // matriz de resultados >> numpad
 unsigned char S = 731; // bordas
+int contJog, played, vezde = 0;
 
 int main(){
-	showMenu(menu, 18);	// menu inicial >> composição final
+	showMenu(menu, tam);	// menu inicial >> composição final
 	
 	return 0;
 }
@@ -72,20 +75,38 @@ void showMenu(int n, int x){
 			composition();
 			break;
 		
-		case 2:
+		case 2: 
 			puts("\n\n");
 			showTabM(TAM, 5, TAM, posS); // tabuleiro visual >> como jogar
 			getch();
 			break;
+			
 		default:
-			showMenu(menu, 18);	// menu inicial
+			showMenu(menu, tam);	// menu inicial
 			break;
 	}
 }
-
+	// composição final >> solo/dupla
 void composition(){
-	char res, a[2] = {'1', '2'};
-	int contJog, played, vezde = 0;
+	int op;
+	do{
+		puts("\n\n");
+		tab(19);
+	
+	printf("\n\t%c\t\t  %c\n"
+		"\t%c 2 - DUPLA|JOGAR %c\n"
+		"\t%c 1 -  SOLO|JOGAR %c\n"	
+		"\t%c 0 -        SAIR %c\n"
+		"\t%c\t\t  %c\n", S, S, S, S, S, S, S, S, S, S);
+			
+		tab(19);
+	
+	printf("\n\t  ");
+	scanf(" %i", &op);
+	
+	system("cls");	
+	}while(op < 1 || op > 2); // 
+	
 	do{
 		contJog = 1;
 		for(i = 0; i < 9; i++) posS[i] = ' '; // zerar o valor contido nas posS
@@ -94,10 +115,87 @@ void composition(){
 			puts("\n\n");
 			showTab(TAM, 5, TAM, posS); // tabuleiro jogável	
 			
-			printf("\n\n\tMarque [1-9]: ");
-			scanf(" %i", &played);	
+			if(vezde % 2 == 0){
+				printf("\n\n\t     'X'\n\tMarque [1-9]: ");	
+				scanf(" %i", &played);	
+			} 
+			else{
+				if(op == 2){ // condição somente funcionará enquando op RECEBE 2
+					printf("\n\n\t     'O'\n\tMarque [1-9]: ");
+					scanf(" %i", &played);
+				}else{
+					dots(3); // 
+					
+						// condições para CPU ganhar
+							// 0/3/6	
+				    if(posS[0] == 'O' && posS [1] == 'O' && posS[2] == ' ') played = 3;
+					else if(posS[3] == 'O' && posS[4] == 'O' && posS[5] == ' ') played = 6;	
+					else if(posS[6] == 'O' && posS[7] == 'O' && posS[8] == ' ') played = 9;	
+					else if(posS[0] == 'O' && posS[1] == ' ' && posS[2] == 'O') played = 2;
+					else if(posS[3] == 'O' && posS[4] == ' ' && posS[5] == 'O') played = 5;	
+					else if(posS[6] == 'O' && posS[7] == ' ' && posS[8] == 'O') played = 8;	
+					else if(posS[0] == ' ' && posS[1] == 'O' && posS[2] == 'O') played = 1;
+					else if(posS[3] == ' ' && posS[4] == 'O' && posS[5] == 'O') played = 4;	
+					else if(posS[6] == ' ' && posS[7] == 'O' && posS[8] == 'O') played = 7;
+							// 0/1/2
+					else if(posS[0] == 'O' && posS[3] == 'O' && posS[6] == ' ') played = 7;
+					else if(posS[1] == 'O' && posS[4] == 'O' && posS[7] == ' ') played = 8;
+					else if(posS[2] == 'O' && posS[5] == 'O' && posS[8] == ' ') played = 9;			
+					else if(posS[0] == 'O' && posS[3] == ' ' && posS[6] == 'O') played = 4;
+					else if(posS[1] == 'O' && posS[4] == ' ' && posS[7] == 'O') played = 5;
+					else if(posS[2] == 'O' && posS[5] == ' ' && posS[8] == 'O') played = 6;			
+					else if(posS[0] == ' ' && posS[3] == 'O' && posS[6] == 'O') played = 1;
+					else if(posS[1] == ' ' && posS[4] == 'O' && posS[7] == 'O') played = 2;
+					else if(posS[2] == ' ' && posS[5] == 'O' && posS[8] == 'O') played = 3;
+							// 0/0/0
+					else if(posS[0] == 'O' && posS[4] == 'O' && posS[8] == ' ') played = 9;
+					else if(posS[0] == 'O' && posS[4] == ' ' && posS[8] == 'O') played = 5;
+					else if(posS[0] == ' ' && posS[4] == 'O' && posS[8] == 'O') played = 1;
+							// 2/2/2
+					else if(posS[2] == 'O' && posS[4] == 'O' && posS[6] == ' ') played = 7;
+					else if(posS[2] == 'O' && posS[4] == ' ' && posS[6] == 'O') played = 5;
+					else if(posS[2] == ' ' && posS[4] == 'O' && posS[6] == 'O') played = 3;
+				
+						// condições para CPU ganhar
+							// 0/3/6	
+					else if(posS[0] == 'X' && posS[1] == 'X' && posS[2] == ' ') played = 3;
+					else if(posS[3] == 'X' && posS[4] == 'X' && posS[5] == ' ') played = 6;	
+					else if(posS[6] == 'X' && posS[7] == 'X' && posS[8] == ' ') played = 9;	
+					else if(posS[0] == 'X' && posS[1] == ' ' && posS[2] == 'X') played = 2;
+					else if(posS[3] == 'X' && posS[4] == ' ' && posS[5] == 'X') played = 5;	
+					else if(posS[6] == 'X' && posS[7] == ' ' && posS[8] == 'X') played = 8;	
+					else if(posS[0] == ' ' && posS[1] == 'X' && posS[2] == 'X') played = 1;
+					else if(posS[3] == ' ' && posS[4] == 'X' && posS[5] == 'X') played = 4;	
+					else if(posS[6] == ' ' && posS[7] == 'X' && posS[8] == 'X') played = 7;
+							// 0/1/2
+					else if(posS[0] == 'X' && posS[3] == 'X' && posS[6] == ' ') played = 7;
+					else if(posS[1] == 'X' && posS[4] == 'X' && posS[7] == ' ') played = 8;
+					else if(posS[2] == 'X' && posS[5] == 'X' && posS[8] == ' ') played = 9;			
+					else if(posS[0] == 'X' && posS[3] == ' ' && posS[6] == 'X') played = 4;
+					else if(posS[1] == 'X' && posS[4] == ' ' && posS[7] == 'X') played = 5;
+					else if(posS[2] == 'X' && posS[5] == ' ' && posS[8] == 'X') played = 6;			
+					else if(posS[0] == ' ' && posS[3] == 'X' && posS[6] == 'X') played = 1;
+					else if(posS[1] == ' ' && posS[4] == 'X' && posS[7] == 'X') played = 2;
+					else if(posS[2] == ' ' && posS[5] == 'X' && posS[8] == 'X') played = 3;
+							// 0/0/0
+					else if(posS[0] == 'X' && posS[4] == 'X' && posS[8] == ' ') played = 9;
+					else if(posS[0] == 'X' && posS[4] == ' ' && posS[8] == 'X') played = 5;
+					else if(posS[0] == ' ' && posS[4] == 'X' && posS[8] == 'X') played = 1;
+							// 2/2/2
+					else if(posS[2] == 'X' && posS[4] == 'X' && posS[6] == ' ') played = 7;
+					else if(posS[2] == 'X' && posS[4] == ' ' && posS[6] == 'X') played = 5;
+					else if(posS[2] == ' ' && posS[4] == 'X' && posS[6] == 'X') played = 3;
+					else{
+						do{
+							srand((unsigned) time(NULL));
+							
+							played = 1 + rand() % 9;	
+						}while(posS[played - 1] != ' ');							
+					}
+				}
+			} 	
 			
-			if(played == 0) showMenu(menu, 18); // retorna ao menu inicial
+			if(played == 0) showMenu(menu, tam); // retorna ao menu inicial
 			if(played < 1 || played > 9) played = 0; // restringe o número de casas
 			else  if(posS[played - 1] != ' ') played = 0; // marca uma casa nula
 			else{
@@ -107,27 +205,29 @@ void composition(){
 				contJog++;
 				vezde++;
 			}
+			
 				// condições para 'X' ganhar
-			if (posS[0] == 'X' && posS [1] == 'X' && posS[2] == 'X') contJog = 11;
-			if (posS[3] == 'X' && posS [4] == 'X' && posS[5] == 'X') contJog = 11;
-			if (posS[6] == 'X' && posS [7] == 'X' && posS[8] == 'X') contJog = 11;
-			if (posS[0] == 'X' && posS [3] == 'X' && posS[6] == 'X') contJog = 11;
-			if (posS[1] == 'X' && posS [4] == 'X' && posS[7] == 'X') contJog = 11;
-			if (posS[2] == 'X' && posS [5] == 'X' && posS[8] == 'X') contJog = 11;
-			if (posS[0] == 'X' && posS [4] == 'X' && posS[8] == 'X') contJog = 11;
-			if (posS[2] == 'X' && posS [4] == 'X' && posS[6] == 'X') contJog = 11;
+			if(posS[0] == 'X' && posS [1] == 'X' && posS[2] == 'X') contJog = 11;
+			if(posS[3] == 'X' && posS [4] == 'X' && posS[5] == 'X') contJog = 11;
+			if(posS[6] == 'X' && posS [7] == 'X' && posS[8] == 'X') contJog = 11;
+			if(posS[0] == 'X' && posS [3] == 'X' && posS[6] == 'X') contJog = 11;
+			if(posS[1] == 'X' && posS [4] == 'X' && posS[7] == 'X') contJog = 11;
+			if(posS[2] == 'X' && posS [5] == 'X' && posS[8] == 'X') contJog = 11;
+			if(posS[0] == 'X' && posS [4] == 'X' && posS[8] == 'X') contJog = 11;
+			if(posS[2] == 'X' && posS [4] == 'X' && posS[6] == 'X') contJog = 11;
 				// condições para 'X' ganhar
-			if (posS[0] == 'O' && posS [1] == 'O' && posS[2] == 'O') contJog = 12;
-			if (posS[3] == 'O' && posS [4] == 'O' && posS[5] == 'O') contJog = 12;
-			if (posS[6] == 'O' && posS [7] == 'O' && posS[8] == 'O') contJog = 12;
-			if (posS[0] == 'O' && posS [3] == 'O' && posS[6] == 'O') contJog = 12;
-			if (posS[1] == 'O' && posS [4] == 'O' && posS[7] == 'O') contJog = 12;
-			if (posS[2] == 'O' && posS [5] == 'O' && posS[8] == 'O') contJog = 12;
-			if (posS[0] == 'O' && posS [4] == 'O' && posS[8] == 'O') contJog = 12;
-			if (posS[2] == 'O' && posS [4] == 'O' && posS[6] == 'O') contJog = 12;
+			if(posS[0] == 'O' && posS [1] == 'O' && posS[2] == 'O') contJog = 12;
+			if(posS[3] == 'O' && posS [4] == 'O' && posS[5] == 'O') contJog = 12;
+			if(posS[6] == 'O' && posS [7] == 'O' && posS[8] == 'O') contJog = 12;
+			if(posS[0] == 'O' && posS [3] == 'O' && posS[6] == 'O') contJog = 12;
+			if(posS[1] == 'O' && posS [4] == 'O' && posS[7] == 'O') contJog = 12;
+			if(posS[2] == 'O' && posS [5] == 'O' && posS[8] == 'O') contJog = 12;
+			if(posS[0] == 'O' && posS [4] == 'O' && posS[8] == 'O') contJog = 12;
+			if(posS[2] == 'O' && posS [4] == 'O' && posS[6] == 'O') contJog = 12;
 		}while(contJog <= 9);
 		
 		showTab(TAM, 5, TAM, posS); // tabuleiro jogável
+		
 		if(contJog == 10) printf("\n\n\tJogo Empatado.");
 		if(contJog == 11) printf("\n\n\tVencedor X!");
 		if(contJog == 12) printf("\n\n\tVencedor O!");	
@@ -136,11 +236,23 @@ void composition(){
 		scanf(" %s", &res);
 	
 		res = toupper(res);
-		if(res == 'N') showMenu(menu, 18); // retorna ao menu inicial
+		if(res == 'N') showMenu(menu, tam); // retorna ao menu inicial
 	}while(res == 'S');
 	
 	getch();
 }
+	// tela de loading
+void dots(int n){
+//	system("cls");
+	printf("\n\n\t");
+	
+	for(i = 0; i < n; i++){
+			printf(". ");
+			Sleep(230);
+	}	
+	Sleep(90);
+}
+
 	// bordas horizontais >> 1
 void tab(int n){
 	printf("\t");
@@ -229,7 +341,7 @@ void help(){
 	
 	tab2(29); // bordas horizontais
 		
-	sleep(8); // pausa de 8 secs 
+	sleep(7); // pausa de 7 secs 
 	
-	showMenu(menu, 18); // retorna ao menu inicial
+	showMenu(menu, tam); // retorna ao menu inicial
 }
